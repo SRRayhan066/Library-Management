72f1857bac7f66f9560a555de436550a389d54c4
SRRayhan066 <shafikulrahman66@gmail.com>
2025-10-14 21:05:13 +0600

    Refactor sidebar and create new pages

--- src/components/app-sidebar/AppSidebar.tsx
+++ src/components/app-sidebar/AppSidebar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Sidebar,
   SidebarContent,
@@ -9,56 +11,21 @@ import {
   SidebarMenuItem,
   SidebarMenuButton,
 } from "@/components/ui/sidebar";
-
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
-  IconLayoutDashboardFilled,
-  IconBookFilled,
-  IconUsersGroup,
-  IconCoinTakaFilled,
-  IconSettings,
-  IconHelpCircleFilled,
-} from "@tabler/icons-react";
-
-const items = [
-  {
-    title: "Dashboard",
-    url: "#",
-    icon: IconLayoutDashboardFilled,
-  },
-  {
-    title: "Books",
-    url: "#",
-    icon: IconBookFilled,
-  },
-  {
-    title: "Members",
-    url: "#",
-    icon: IconUsersGroup,
-  },
-  {
-    title: "Fines & Payments",
-    url: "#",
-    icon: IconCoinTakaFilled,
-  },
-];
-
-const secondaryItems = [
-  {
-    title: "Settings",
-    url: "#",
-    icon: IconSettings,
-  },
-  {
-    title: "Help & Support",
-    url: "#",
-    icon: IconHelpCircleFilled,
-  },
-];
+  primaryItems,
+  secondaryItems,
+} from "@/constant/default-values/SidebarOptions";
+import { NavItemsProps } from "@/types/NavItemsProps";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 export function AppSidebar() {
+  const pathName = usePathname();
+  console.log({ pathName });
   return (
     <Sidebar>
-      <SidebarHeader className="">
+      <SidebarHeader>
         <SidebarMenu>
           <SidebarMenuItem>
             <SidebarMenuButton asChild className="p-1.5">
@@ -73,42 +40,53 @@ export function AppSidebar() {
       </SidebarHeader>
 
       <SidebarContent className="flex flex-col gap-2">
-        <SidebarGroup>
-          <SidebarGroupContent className="flex flex-col gap-2">
-            <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton tooltip={item.title}>
-                    {item.icon && <item.icon />}
-                    <span>{item.title}</span>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-        <SidebarGroup className="mt-auto">
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {secondaryItems.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton tooltip={item.title}>
-                    {item.icon && <item.icon />}
-                    <span>{item.title}</span>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
+        <NavItems items={primaryItems} pathName={pathName} />
+        <NavItems
+          items={secondaryItems}
+          pathName={pathName}
+          className="mt-auto"
+        />
       </SidebarContent>
-      <SidebarFooter className="">
-        <SidebarMenu className="">
-          <SidebarMenuItem className="">
-            <SidebarMenuButton>User Details</SidebarMenuButton>
+
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem className="py-2">
+            <SidebarMenuButton className="h-full flex justify-start">
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <div className="flex-col">
+                <h3 className="font-semibold">John Doe</h3>
+                <p className="text-[12px]">john@gmail.com</p>
+              </div>
+            </SidebarMenuButton>
           </SidebarMenuItem>
         </SidebarMenu>
       </SidebarFooter>
     </Sidebar>
   );
 }
+
+const NavItems = ({ items, pathName = "", className = "" }: NavItemsProps) => (
+  <SidebarGroup className={className}>
+    <SidebarGroupContent className="flex flex-col gap-2">
+      <SidebarMenu>
+        {items.map((item) => (
+          <SidebarMenuItem key={item.title}>
+            <SidebarMenuButton
+              className={`${pathName === item.url && "bg-gray-800"}`}
+              tooltip={item.title}
+              asChild
+            >
+              <Link href={item.url}>
+                {item.icon && <item.icon />}
+                <span>{item.title}</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        ))}
+      </SidebarMenu>
+    </SidebarGroupContent>
+  </SidebarGroup>
+);
---
+++ src/constant/default-values/SidebarOptions.ts
@@ -0,0 +1,44 @@
+import {
+  IconLayoutDashboardFilled,
+  IconBookFilled,
+  IconUsersGroup,
+  IconCoinTakaFilled,
+  IconSettings,
+  IconHelpCircleFilled,
+} from "@tabler/icons-react";
+
+export const primaryItems = [
+  {
+    title: "Dashboard",
+    url: "/dashboard",
+    icon: IconLayoutDashboardFilled,
+  },
+  {
+    title: "Books",
+    url: "/books",
+    icon: IconBookFilled,
+  },
+  {
+    title: "Members",
+    url: "/members",
+    icon: IconUsersGroup,
+  },
+  {
+    title: "Fines & Payments",
+    url: "/fines-and-payments",
+    icon: IconCoinTakaFilled,
+  },
+];
+
+export const secondaryItems = [
+  {
+    title: "Settings",
+    url: "/settings",
+    icon: IconSettings,
+  },
+  {
+    title: "Help & Support",
+    url: "/help-and-support",
+    icon: IconHelpCircleFilled,
+  },
+];
